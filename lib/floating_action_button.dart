ec5df694bd0748d0ac858a80b06ef902d830b703
ioviedodev <ivanoviedo.91@gmail.com>
2022-10-11 16:56:48 -0500

    Handle actions over fav button

--- lib/floating_action_button.dart
+++ lib/floating_action_button.dart
@@ -12,20 +12,42 @@ class FloatingActionButtonCustomize extends StatefulWidget{
 }
 
 class _FloatingActionButtonCustomize extends State<FloatingActionButtonCustomize>{
-  String snackBarText = "Adding to favourite";
-  void onPressedFav(){
-    ScaffoldMessenger.of(context).showSnackBar( SnackBar(content: Text(snackBarText)));
-}
+  String addFav = "Adding to favourite", removeFav = "Removing from favourites";
+  int fabIndex = 0;
+  Icon fab = const Icon(Icons.favorite_border);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return FloatingActionButton(
+    final floatingActionButton= FloatingActionButton(
       backgroundColor: const Color(0XFF11DA53),
       mini: true,
       tooltip: "Fav",
       onPressed: onPressedFav,
-      child: Icon(Icons.favorite_border),
+
+      child: fab,
     );
+
+    return floatingActionButton;
+
+  }
+
+
+  void onPressedFav(){
+      setState(() {
+        if(fabIndex != 0)
+          {
+            fab = const Icon(Icons.favorite_border);
+            fabIndex=0;
+            ScaffoldMessenger.of(context).showSnackBar( SnackBar(content: Text(removeFav)));
+          }
+        else
+          {
+            fabIndex=1;
+            fab = const Icon(Icons.favorite_sharp);
+            ScaffoldMessenger.of(context).showSnackBar( SnackBar(content: Text(addFav)));
+          }
+
+      });
   }
 }
