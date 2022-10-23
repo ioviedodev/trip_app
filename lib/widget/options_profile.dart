4f703a63d361b718a9bc0611f6ba247ad4d14915
ioviedodev <ivanoviedo.91@gmail.com>
2022-10-22 22:24:24 -0500

    adding effects to icon option profile

--- lib/widget/options_profile.dart
+++ lib/widget/options_profile.dart
@@ -13,7 +13,13 @@ class OptionsProfile extends StatefulWidget{
 class _OptionsProfile extends State<OptionsProfile>{
 
   Color colorBackground=Colors.white;
-  Color iconColor = Colors.indigo;
+  static Color iconColor = Colors.indigo;
+
+  Icon iconOption1 = Icon(color: iconColor, Icons.bookmark_add_outlined);
+  Icon iconOption2 = Icon(color: iconColor, Icons.history);
+  Icon iconOption3 = Icon(color: iconColor, Icons.add_a_photo);
+  Icon iconOption4 = Icon(color: iconColor, Icons.email_outlined);//person
+  Icon iconOption5 = Icon(color: iconColor, Icons.person);
 
   @override
   Widget build(BuildContext context) {
@@ -22,34 +28,45 @@ class _OptionsProfile extends State<OptionsProfile>{
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         Expanded(child: FloatingActionButton(
+          splashColor: Colors.deepOrange,
+          tooltip: 'Bookmark',
           backgroundColor: colorBackground,
           mini: true,
           onPressed: onPressed,
-          child:  Icon(color: iconColor, Icons.bookmark_add_outlined),
+          child:  iconOption1,
         )),
         Expanded(child: FloatingActionButton(
+          splashColor: Colors.deepOrange,
+          tooltip: 'History',
           backgroundColor:  colorBackground,
           mini: true,
           onPressed: onPressed,
-          child:  Icon(color: iconColor, Icons.gif_box_outlined),
+          child:  iconOption2,
         )),
         Expanded(child: FloatingActionButton(
-          backgroundColor:  colorBackground,
-          mini: false,
-          onPressed: onPressed,
-          child:  Icon(color: iconColor, Icons.add_a_photo),
-        )),
-        Expanded(child: FloatingActionButton(
+          elevation: 20.0,
+          splashColor: Colors.deepOrange,
+          tooltip: 'Photo',
           backgroundColor:  colorBackground,
           mini: true,
           onPressed: onPressed,
-          child:  Icon(color: iconColor, Icons.email_outlined),
+          child:  iconOption3,
         )),
         Expanded(child: FloatingActionButton(
+          splashColor: Colors.deepOrange,
+          tooltip: 'Mail',
           backgroundColor:  colorBackground,
           mini: true,
           onPressed: onPressed,
-          child:  Icon(color: iconColor, Icons.person),
+          child:  iconOption4,
+        )),
+        Expanded(child: FloatingActionButton(
+          splashColor: Colors.deepOrange,
+          tooltip: 'Profile',
+          backgroundColor:  colorBackground,
+          mini: true,
+          onPressed: onPressed,
+          child:  iconOption5,
         )),
 
       ],
@@ -63,5 +80,6 @@ class _OptionsProfile extends State<OptionsProfile>{
       //
       // }
     }
+
 
 }
